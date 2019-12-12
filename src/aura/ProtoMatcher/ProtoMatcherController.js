c27d375dda93b172ea6f7f6eede5a9484fa171f1
Ronan Williams <ronan@ceterna.com>
2019-12-12 09:11:12 +0000

    12.12.2019 2

--- src/aura/ProtoMatcher/ProtoMatcherController.js
+++ src/aura/ProtoMatcher/ProtoMatcherController.js
@@ -31,8 +31,54 @@
 
                     $C.set('v.transactions',transactions);
                 } else {
+                    transactions.forEach(function(transaction){
+                        transaction.SuggestedMatches__c = [];
 
-                    //todo: js client side match logic
+                        var transDate = new Date(transaction.TransactionDate__c);
+
+                        transaction.Identifier =
+                            // add the transaction date to the identifier string
+                            '::' + new Date(transDate).setDate(transDate.getDate() - 1) +
+                            '::' + transDate +
+                            '::' + new Date(transDate).setDate(transDate.getDate() + 1) +
+
+                            // add the vendor name and transaction description to the identifier string
+                            '::' + transaction.Vendor__c.toLowerCase().replace(' ','') + '::' + transaction.Description__c + '::' +
+
+                            // add the transaction ISO currency code to the string
+                            '::' + transaction.ISOCode__c + '::' + transaction.Amount__c + '::';
+                    });
+
+                    var expenses = response.getReturnValue()['expenses'];
+                    if (expenses){
+                        expenses.forEach(function(expense){
+
+                            transactions.forEach(function(transaction){
+
+                                console.log('currency',expense.salestrip__TransactionCurrency__c );
+
+                                // do not attempt to match the Expense with the Bank Transaction if the Expense
+                                // has already been rejected as a match
+                                if (!transaction.RejectedMatches__c || transaction.RejectedMatches__c.indexOf(expense.Id) === -1){
+
+                                    // align match attributes by checking if the Expense properties occur in the transaction identifier key string
+                                    var amountMatch      = transaction.Identifier.includes('::' + expense.salestrip__Amount__c + '::');
+                                    var currencyMatch    = transaction.Identifier.includes('::' + expense.salestrip__TransactionCurrency__c + '::');
+                                    var dateMatch        = transaction.Identifier.includes('::' + new Date(expense.salestrip__Date__c) + '::');
+                                    var vendorMatch      = $C.get('v.ignoreVendor') ? true : transaction.Identifier.includes(expense.salestrip__Vendor__c.toLowerCase().replace(' ',''));
+                                    var descriptionMatch = !$C.get('v.ignoreVendor') && !vendorMatch && $C.get('v.compareDescriptions') && expense.salestrip__Description__c ?
+                                        transaction.Identifier.includes(expense.salestrip__Description__c.toLowerCase().replace(' ','')) : true;
+
+                                    // if all relevant attributes match the Bank Transaction, add the record as a suggested match
+                                    if (amountMatch && currencyMatch && dateMatch && vendorMatch && descriptionMatch){
+                                        transaction.SuggestedMatches__c.push(expense);
+                                    }
+                                }
+                            });
+
+
+                        });
+                    }
 
                     $C.set('v.transactions',transactions);
 
@@ -52,8 +98,14 @@
         $C.set('v.activeId','');
     },
     rejectMatch : function($C,$E,$H){
-        console.log($E.currentTarget.dataset.trans);
 
+        var data            = $E.currentTarget.dataset;
+        var transactions    = $C.get('v.transactions');
+        console.log('trans',transactions[data.transindex]);
+        transactions[data.transindex].SuggestedMatches__c.splice(data.matchindex,1);
+        $C.set('v.transactions',transactions);
+
+        // todo: pass rejection to apex method
 
     }
 
